4da246cf1d8738590cc1183f558a97f4b8266bb7
buriedsignals <tom.vaillant@icloud.com>
2021-01-22 11:56:53 +0100

    font still not working

--- posts/d3test/components/custom-d3-component.js
+++ posts/d3test/components/custom-d3-component.js
@@ -2,31 +2,43 @@ const React = require('react');
 const D3Component = require('idyll-d3-component');
 const d3 = require('d3');
 
-const size = 600;
+class CircleWave extends D3Component {
 
-class CustomD3Component extends D3Component {
-  // initialize(node, props) {
-  //   const svg = (this.svg = d3.select(node).append('svg'));
-  //   svg
-  //     .attr('viewBox', `0 0 ${size} ${size}`)
-  //     .style('width', '100%')
-  //     .style('height', 'auto');
+  initialize(node, props) {
+    const svg = d3.select(node).append('svg');
+    const width = node.getBoundingClientRect().width;
+    const height = width;
 
-  //   svg
-  //     .append('circle')
-  //     .attr('r', 20)
-  //     .attr('cx', Math.random() * size)
-  //     .attr('cy', Math.random() * size);
-  // }
+    svg.attr('width', width);
+    svg.attr('height', height);
 
-  // update(props, oldProps) {
-  //   this.svg
-  //     .selectAll('circle')
-  //     .transition()
-  //     .duration(750)
-  //     .attr('cx', Math.random() * size)
-  //     .attr('cy', Math.random() * size);
-  // }
+    const angles = d3.range(0, 2 * Math.PI, Math.PI / 200);
+
+    const path = svg.append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")")
+        .attr("fill", "none")
+        .attr("stroke-width", 3)
+        .attr("stroke-linejoin", "round")
+      .selectAll("path")
+      .data(["cyan", "magenta", "yellow"])
+      .enter().append("path")
+        .attr("stroke", function(d) { return d; })
+        .style("mix-blend-mode", "darken")
+        .datum(function(d, i) {
+          return d3.radialLine()
+              .curve(d3.curveLinearClosed)
+              .angle(function(a) { return a; })
+              .radius(function(a) {
+                var t = d3.now() / 1000;
+                return width / 3 + Math.cos(a * 8 - i * 2 * Math.PI / 3 + t) * Math.pow((1 + Math.cos(a - t)) / 2, 3) * width / 20;
+              });
+        });
+
+    d3.timer(function() {
+      path.attr("d", function(d) {
+        return d(angles);
+      });
+    });
+  }
 }
-
-module.exports = CustomD3Component;
+module.exports = CircleWave;
